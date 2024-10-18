5e6f45ad65d0c69e1e1ef6dc2efc3811032e8cb9
Kariuki Karanja <kariukimarkbrian@gmail.com>
2024-10-18 15:42:16 +0300

    :sparkles:

--- src/Auth/Signin.jsx
+++ src/Auth/Signin.jsx
@@ -1,48 +1,50 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const SignInForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate(); // Initialize useNavigate hook
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-
-
         try {
-
-
             const response = await axios.post(
                 'http://127.0.0.1:3000/users/sign_in',
                 { email, password },
                 { withCredentials: true }
             );
 
-            if (response && response.data && response.data.success)
-                console.log("sign in successful")
+            if (response && response.data && response.data.success) {
+
+                const userId = response.data.user.id;
 
 
+                localStorage.setItem('user_id', userId);
+
+                console.log("Sign in successful, user ID stored");
+
+
+               
+
+
+                navigate('/');
+            }
         } catch (error) {
             console.error(error);
-
-        } finally {
-
         }
     };
 
-
     return (
-        <div >
-
-
-
+        <div>
             <div className="p-4 rounded">
                 <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="email">
-                        <Form.Label >Email address</Form.Label>
+                        <Form.Label>Email address</Form.Label>
                         <Form.Control
                             type="email"
                             value={email}
@@ -52,7 +54,7 @@ const SignInForm = () => {
                         />
                     </Form.Group>
                     <Form.Group controlId="password">
-                        <Form.Label >Password</Form.Label>
+                        <Form.Label>Password</Form.Label>
                         <Form.Control
                             type={showPassword ? 'text' : 'password'}
                             value={password}
@@ -71,20 +73,12 @@ const SignInForm = () => {
                         </div>
                     </Form.Group>
 
-
-                    : <Button
-                        type="submit"
-
-
-                    >
+                    <Button type="submit">
                         Sign In
                     </Button>
-
-
                 </Form>
             </div>
         </div>
-
     );
 };
 
