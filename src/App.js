3d3f329d56a04c1b149660d83eb65cdfcdb420a8
Kariuki Karanja <kariukimarkbrian@gmail.com>
2024-10-18 15:42:06 +0300

    :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
-import SignInForm from "./Auth/Signin";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import SignIn from "./Auth/Signin";
 import SignUp from "./Auth/Signup";
-// import Meeting from "./Components/Meeting";
-// import Sales from "./Components/Sales"
-// import Messager from "./Components/Messager";
-// import Calendar from "./Components/Calendar"
-// import DocumentPage from "./Components/Document";
+import Home from "./Components/Home";
+import TaskMeetingCalendar from "./Components/TaskMeetingCalendar";
+import DocumentPage from "./Components/Document";
+import Navigation from "./Components/Navigation";
+import Chat from "./Components/chat";
+import VideoCall from "./Components/VideoCall";
 
 function App() {
-  return (
-    <div>
-    <SignUp />
-  <SignInForm />
-    </div>
-  )
+    const isLoggedIn = !!localStorage.getItem('user_id');
+
+    return (
+        <Router>
+            <div>
+                {isLoggedIn && <Navigation />}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/documents" element={isLoggedIn ? <DocumentPage /> : <Navigate to="/signin" />} />
+                    <Route path="/taskmanager" element={isLoggedIn ? <TaskMeetingCalendar /> : <Navigate to="/signin" />} />
+                    <Route path="/chat" element={isLoggedIn ? <Chat /> : <Navigate to="/signin" />} />
+                    <Route path="/video-call" element={isLoggedIn ? <VideoCall /> : <Navigate to="/signin" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
